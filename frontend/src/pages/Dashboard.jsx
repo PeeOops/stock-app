49259a24527697d12cfe9932d3e49fb2c10fa72b
PeeOops <liem.richiee@gmail.com>
2025-08-08 10:29:41 +0700

    Installed FontAwesomeIcon and making Dashboard UI Page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Dashboard = () => {
     return(
         <>
-            <p className="text-red-400">Hello world</p>
+            <div className="grid grid-cols-[1fr_3fr] h-screen font-mono">
+                {/* Navigation panel */}
+                <div className="flex flex-col items-center bg-slate-800 text-white pt-8">
+                    <FontAwesomeIcon icon={faTruck} className="text-3xl mb-4" />
+                    <p>PeeDegrees</p>
+                    <p className="text-sm">peedegrees@gmail.com</p>
+                </div>
+                {/* Content */}
+                <div className="bg-white">
+                    <p>tes</p>
+                </div>
+            </div>
         </>
     )  
 }
