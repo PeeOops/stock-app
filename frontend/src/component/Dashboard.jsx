174246430e8641e7ffb3f5f8259515d31e9c7df3
PeeOops <liem.richiee@gmail.com>
2025-08-08 14:52:43 +0700

    Added Home header and working on Dashboard component

--- frontend/src/component/Dashboard.jsx
+++ frontend/src/component/Dashboard.jsx
@@ -1,7 +1,13 @@
 const Dashboard = () => {
     return(
-        <div>
-            <h1>Dashboard</h1>
+        <div className="bg-teal-400 p-6 h-screen">
+            {/* Overview */}
+            <div className="flex flex-col bg-white p-2">
+                <h1 className="text-xl">Overview</h1>
+                <div className="flex flex-row">
+
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { faLayerGroup, faList, faTruck, faUser, faWarehouse } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faLayerGroup, faList, faRightFromBracket, faTruck, faUser, faWarehouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Dashboard from "../component/Dashboard";
 import { useState } from "react";
@@ -26,20 +26,20 @@ const Home = () => {
                     <p className="text-sm">peedegrees@gmail.com</p>
 
                     {/* Navigation buttons */}
-                    <div className="flex flex-col justify-center ml-16 mt-8 gap-4 w-full">
-                        <div id="Dashboard" onClick={handleClickNavigation} role="button" className="flex flex-row items-center gap-2 hover:bg-white hover:text-slate-800 cursor-pointer p-1 rounded-sm">
+                    <div className="flex flex-col justify-center mt-8 gap-4 w-full">
+                        <div id="Dashboard" onClick={handleClickNavigation} role="button" className={`flex flex-row items-center gap-2 hover:bg-teal-400 hover:text-slate-800 cursor-pointer py-1 px-6 md:px-12 ${navigation === "Dashboard" && "bg-teal-400 text-slate-800"} `}>
                             <FontAwesomeIcon icon={faLayerGroup} />
                             <p>Dashboard</p>
                         </div>
-                        <div id="Trucks" onClick={handleClickNavigation} role="button" className="flex flex-row items-center gap-2 hover:bg-white hover:text-slate-800 cursor-pointer p-1 rounded-sm">
+                        <div id="Trucks" onClick={handleClickNavigation} role="button" className={`flex flex-row items-center gap-2 hover:bg-teal-400 hover:text-slate-800 cursor-pointer py-1 px-6 md:px-12 ${navigation === "Trucks" && "bg-teal-400 text-slate-800"} `}>
                             <FontAwesomeIcon icon={faList} />
                             <p>Trucks</p>
                         </div>
-                        <div id="Inventories" onClick={handleClickNavigation} role="button" className="flex flex-row items-center gap-2 hover:bg-white hover:text-slate-800 cursor-pointer p-1 rounded-sm">
+                        <div id="Inventories" onClick={handleClickNavigation} role="button" className={`flex flex-row items-center gap-2 hover:bg-teal-400 hover:text-slate-800 cursor-pointer py-1 px-6 md:px-12 ${navigation === "Inventories" && "bg-teal-400 text-slate-800"} `}>
                             <FontAwesomeIcon icon={faWarehouse} />
                             <p>Inventories</p>
                         </div>
-                        <div id="Suppliers" onClick={handleClickNavigation} role="button" className="flex flex-row items-center gap-2 hover:bg-white hover:text-slate-800 cursor-pointer p-1 rounded-sm">
+                        <div id="Suppliers" onClick={handleClickNavigation} role="button" className={`flex flex-row items-center gap-2 hover:bg-teal-400 hover:text-slate-800 cursor-pointer py-1 px-6 md:px-12 ${navigation === "Suppliers" && "bg-teal-400 text-slate-800"} `}>
                             <FontAwesomeIcon icon={faUser} />
                             <p>Suppliers</p>
                         </div>
@@ -47,9 +47,22 @@ const Home = () => {
                      <div></div>
                 </div>
                 {/* Content */}
-                {
-                    navigation === "Dashboard" ? <Dashboard /> : navigation === "Trucks" ? <Trucks /> : navigation === "Inventories" ? <Inventories /> : navigation === "Suppliers" ? <Suppliers /> : <p>Error</p>
-                }
+                <div className="flex flex-col">
+                    {/* Header */}
+                    <div className="flex flex-row items-center justify-between border-b-3 border-gray-400 p-4">
+                        <h1 className="text-xl">Welcome back, PeeDegrees!</h1>
+                        <div className="flex flex-row items-center gap-4">
+                            <FontAwesomeIcon icon={faBell} className="text-lg cursor-pointer" arial-label="Notification" />
+                            <FontAwesomeIcon icon={faRightFromBracket} className="text-lg cursor-pointer" arial-label="Logout" />
+                        </div>
+                        
+                    </div>
+                    {/* Components */}
+                    {
+                        navigation === "Dashboard" ? <Dashboard /> : navigation === "Trucks" ? <Trucks /> : navigation === "Inventories" ? <Inventories /> : navigation === "Suppliers" ? <Suppliers /> : <p>Error</p>
+                    }
+                </div>
+
             </div>
         </>
     )  
