4a63dd0931eb1aa893320979b1db2674659075f9
PeeOops <liem.richiee@gmail.com>
2025-08-08 15:25:47 +0700

    Added overview UI for Dashboard

--- frontend/src/component/Dashboard.jsx
+++ frontend/src/component/Dashboard.jsx
@@ -1,11 +1,29 @@
+import { faBox, faTruck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Dashboard = () => {
     return(
-        <div className="bg-teal-400 p-6 h-screen">
+        <div className="bg-teal-400 p-6 h-screen font-mono">
             {/* Overview */}
-            <div className="flex flex-col bg-white p-2">
-                <h1 className="text-xl">Overview</h1>
-                <div className="flex flex-row">
-
+            <div className="flex flex-col gap-2 bg-white rounded-md shadow-md shadow-gray-600 p-2">
+                <h1 className="text-2xl">Overview</h1>
+                <div className="flex flex-row justify-evenly gap-2">
+                    <div className="flex flex-col border-2 border-black rounded-md text-xl py-2 px-8">
+                        <p>22 <FontAwesomeIcon icon={faTruck} /></p>
+                        <p>Total Trucks</p>
+                    </div>
+                    <div className="flex flex-col border-2 border-black rounded-md text-xl py-2 px-8">
+                        <p>22 <FontAwesomeIcon icon={faBox} /></p>
+                        <p>Total Items</p>
+                    </div>
+                    <div className="flex flex-col border-2 border-black rounded-md text-xl py-2 px-8">
+                        <p>22 <FontAwesomeIcon icon={faTruck} /></p>
+                        <p>Total Trucks</p>
+                    </div>
+                    <div className="flex flex-col border-2 border-black rounded-md text-xl py-2 px-8">
+                        <p>22 <FontAwesomeIcon icon={faTruck} /></p>
+                        <p>Total Trucks</p>
+                    </div>
                 </div>
             </div>
         </div>
